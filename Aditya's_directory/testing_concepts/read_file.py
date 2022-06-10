1e419e63b65b3cfa80559084495837764d950d22
aditya <adityaved95@gmail.com>
2022-06-10 12:44:09 +0530

    Initial Design

---
+++ Aditya's_directory/testing_concepts/read_file.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+file_name = "/Aditya's_directory/testing_concepts/company.pkl"
+objects = pd.read_pickle(file_name)
+print(objects)
--- testing_concepts/read_file.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-file_name = "/Users/adityaved/prj_ws/fy_s2_py_prj/testing_concepts/company.pkl"
-objects = pd.read_pickle(file_name)
-print(objects)
